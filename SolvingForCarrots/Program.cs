60e6b7076aa9441623e1f31a813181b229e558a0
Erik <erik.ra@gmail.com>
2017-04-23 14:30:22 +0200

    Add project files.

---
+++ SolvingForCarrots/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SolvingForCarrots
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int lines = int.Parse(input[0]);
+            for (int i = 0; i < lines; i++)
+            {
+                Console.ReadLine();
+            }
+            Console.WriteLine(input[1]);
+        }
+    }
+}
